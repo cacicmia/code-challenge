78c4e9c3645f806a973d21eff76022adccb0b73b
Mia Ćaćić <cacic.miaa@gmail.com>
2024-07-22 08:22:29 +0200

    refactor: mapHandler tests

--- src/__tests__/MapHandler.test.ts
+++ src/__tests__/MapHandler.test.ts
@@ -1,194 +1,178 @@
 import { mapHandler } from "@/app/service/mapHandler";
 import { MapValidationError } from "@/types/ValidationError";
+const successScenarios = [
+  {
+    name: "should return empty value when provided with an empty value",
+    input: "",
+    result: {
+      path: "",
+      letters: "",
+    },
+  },
+  {
+    name: "should return the expected result for the basic example",
+    input: `
+    @---A---+
+            |
+    x-B-+   C
+        |   |
+        +---+`,
+    result: {
+      letters: "ACB",
+      path: "@---A---+|C|+---+|+-B-x",
+    },
+  },
 
-describe("valid maps tests", () => {
-  it("should return empty value when provided with an empty value", () => {
-    const { path, letters } = mapHandler("");
+  {
+    name: "should go straight through the intersections",
+    input: `
+    @
+    | +-C--+
+    A |    |
+    +---B--+
+      |      x
+      |      |
+      +---D--+`,
+    result: {
+      letters: "ABCD",
+      path: "@|A+---B--+|+--C-+|-||+---D--+|x",
+    },
+  },
 
-    expect(path).toBe("");
-    expect(letters).toBe("");
-  });
-
-  it("should return the expected result for the basic example", () => {
-    const { path, letters } = mapHandler(
-      `
-  @---A---+
-          |
-  x-B-+   C
-      |   |
-      +---+`
-    );
-
-    expect(letters).toBe("ACB");
-    expect(path).toBe("@---A---+|C|+---+|+-B-x");
-  });
-
-  it("should go straight through the intersections", () => {
-    const { path, letters } = mapHandler(
-      `
-      @
-      | +-C--+
-      A |    |
-      +---B--+
-        |      x
-        |      |
-        +---D--+`
-    );
-
-    expect(letters).toBe("ABCD");
-    expect(path).toBe("@|A+---B--+|+--C-+|-||+---D--+|x");
-  });
-
-  it("should be able to find letters on turns", () => {
-    const { path, letters } = mapHandler(
-      `
+  {
+    name: "should be able to find letters on turns",
+    input: `
       @---A---+
               |
       x-B-+   |
           |   |
-          +---C`
-    );
+          +---C`,
+    result: {
+      letters: "ACB",
+      path: "@---A---+|||C---+|+-B-x",
+    },
+  },
 
-    expect(letters).toBe("ACB");
-    expect(path).toBe("@---A---+|||C---+|+-B-x");
-  });
+  {
+    name: "shouldn't collect a letter twice",
+    input: `
+    +-O-N-+
+    |     |
+    |   +-I-+
+@-G-O-+ | | |
+    | | +-+ E
+    +-+     S
+            |
+            x`,
+    result: {
+      letters: "GOONIES",
+      path: "@-G-O-+|+-+|O||+-O-N-+|I|+-+|+-I-+|ES|x",
+    },
+  },
+  {
+    name: "should keep direction in compact space",
+    input: `
+    +-L-+
+    |  +A-+
+   @B+ ++ H
+    ++    x
+   `,
+    result: {
+      letters: "BLAH",
+      path: "@B+++B|+-L-+A+++A-+Hx",
+    },
+  },
+  {
+    name: "should ignore the stuff after the end of path",
+    input: `
+    @-A--+
+         |
+         +-B--x-C--D`,
+    result: {
+      letters: "AB",
+      path: "@-A--+|+-B--x",
+    },
+  },
+];
+describe.each(successScenarios)("valid maps tests", (scenario) => {
+  it(scenario.name, () => {
+    const { path, letters } = mapHandler(scenario.input);
 
-  it("shouldn't collect a letter twice", () => {
-    const { path, letters } = mapHandler(
-      `
-      +-O-N-+
-      |     |
-      |   +-I-+
-  @-G-O-+ | | |
-      | | +-+ E
-      +-+     S
-              |
-              x`
-    );
-
-    expect(letters).toBe("GOONIES");
-    expect(path).toBe("@-G-O-+|+-+|O||+-O-N-+|I|+-+|+-I-+|ES|x");
-  });
-
-  it("should keep direction in compact space", () => {
-    const { path, letters } = mapHandler(
-      `
-      +-L-+
-      |  +A-+
-     @B+ ++ H
-      ++    x
-     `
-    );
-
-    expect(letters).toBe("BLAH");
-    expect(path).toBe("@B+++B|+-L-+A+++A-+Hx");
-  });
-
-  it("should ignore the stuff after the end of path", () => {
-    const { path, letters } = mapHandler(
-      `
-      @-A--+
-           |
-           +-B--x-C--D`
-    );
-
-    expect(letters).toBe("AB");
-    expect(path).toBe("@-A--+|+-B--x");
+    expect(path).toBe(scenario.result.path);
+    expect(letters).toBe(scenario.result.letters);
   });
 });
-describe("invalid maps", () => {
-  it("should fail when missing start character", () => {
-    expect(() =>
-      mapHandler(
-        `
-     -A---+
-          |
-  x-B-+   C
-      |   |
-      +---+
-`
-      )
-    ).toThrow(MapValidationError);
-  });
-  it("should fail when missing end character", () => {
-    expect(() =>
-      mapHandler(
-        `
-   @--A---+
-          |
-    B-+   C
-      |   |
-      +---+
-`
-      )
-    ).toThrow(MapValidationError);
-  });
 
-  it("should fail when having multiple start characters", () => {
-    expect(() =>
-      mapHandler(
-        `
+const failedScenarios = [
+  {
+    name: "should fail when missing start character",
+    input: `
+    -A---+
+         |
+ x-B-+   C
+     |   |
+     +---+
+`,
+  },
+  {
+    name: "should fail when missing end character",
+    input: `
+    @--A---+
+           |
+     B-+   C
+       |   |
+       +---+
+ `,
+  },
+
+  {
+    name: "should fail when having multiple start characters",
+    input: `
    @--A-@-+
           |
   x-B-+   C
       |   |
       +---+
-`
-      )
-    ).toThrow(MapValidationError);
-  });
+`,
+  },
+  {
+    name: "should fail when having fork in the path",
+    input: `
+    x-B
+      |
+@--A---+
+      |
+ x+   C
+  |   |
+  +---+
 
-  it("should fail when having fork in the path", () => {
-    expect(() =>
-      mapHandler(
-        `
-        x-B
-          |
-   @--A---+
-          |
-     x+   C
-      |   |
-      +---+
+`,
+  },
+  {
+    name: "should fail when having a broken path",
+    input: `
+    @--A-+
+         |
+          
+         B-x
+ `,
+  },
+  {
+    name: "should fail when having multiple end paths",
+    input: `
+x-B-@-A-x
+ `,
+  },
+  {
+    name: "should fail when having a fake turn",
+    input: `
+ @-A-+-B-x
 
-`
-      )
-    ).toThrow(MapValidationError);
-  });
-
-  it("should fail when having a broken path", () => {
-    expect(() =>
-      mapHandler(
-        `
-   @--A-+
-        |
-         
-        B-x
-`
-      )
-    ).toThrow(MapValidationError);
-  });
-
-  it("should fail when having multiple starting paths", () => {
-    expect(() =>
-      mapHandler(
-        `
-   @--A-+
-        |
-         
-        B-x
-`
-      )
-    ).toThrow(MapValidationError);
-  });
-
-  it("should fail when having a fake turn", () => {
-    expect(() =>
-      mapHandler(
-        `
-      @-A-+-B-x
-
-`
-      )
-    ).toThrow(MapValidationError);
+ `,
+  },
+];
+describe.each(failedScenarios)("invalid maps", (scenario) => {
+  it(scenario.name, () => {
+    expect(() => mapHandler(scenario.input)).toThrow(MapValidationError);
   });
 });
